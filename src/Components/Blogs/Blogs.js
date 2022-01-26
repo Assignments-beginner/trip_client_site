d174c03bd41ffd8c5ef9af6f25c5406de175cb32
avishekataur <avishekataur@gmail.com>
2022-01-26 19:26:29 +0600

    half_tips

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -71,17 +71,23 @@ const Blogs = () => {
             Travel Tips
           </h5>
           <aside>
-            {/* <Button variant="outline-secondary w-100">GET INSPIRED</Button> */}
-            <>
+            {/* 1//////// */}
+            <div>
               <button onClick={handleShow} className="tipsButton">
-                GET INSPIRED
+                <span>GET INSPIRED</span>
               </button>
               <Modal show={show} onHide={handleClose}>
                 <Modal.Body>
                   <div className="d-flex justify-content-center">
-                    <i style={{ fontSize: "60px",}} className="my-3 far fa-lightbulb"></i>
+                    <i
+                      style={{ fontSize: "60px" }}
+                      className="my-3 far fa-lightbulb"
+                    ></i>
                   </div>
-                  <p style={{ fontSize: "20px", textAlign: "center" }}>
+                  <p
+                    className="px-5 pb-4"
+                    style={{ fontSize: "20px", textAlign: "justify" }}
+                  >
                     Planning a trip and being on the road is a lot easier than
                     you think. It’s the decision to take that step out the door
                     that is the hardest part. It requires the most change.
@@ -89,7 +95,95 @@ const Blogs = () => {
                   </p>
                 </Modal.Body>
               </Modal>
-            </>
+            </div>
+            {/* 2//////// */}
+            <div>
+              <button onClick={handleShow} className="tipsButton">
+                <span>HOW TO SAVE FOR ANY TRIP</span>
+              </button>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Body>
+                  <div className="d-flex justify-content-center">
+                    <i
+                      style={{ fontSize: "60px" }}
+                      className="my-3 far fa-lightbulb"
+                    ></i>
+                  </div>
+                  <p
+                    className="px-5 pb-4"
+                    style={{ fontSize: "20px", textAlign: "justify" }}
+                  >
+                    The key to any trip is being smart with your money – and
+                    that starts even before you hit the road! It’s important to
+                    know how to save and plan your trip so you don’t find
+                    yourself running out of money on the road. Here are my best
+                    tips and tricks on how to save money for any trip -even if
+                    you don’t make a ton of money. I’ve found tons of ways to
+                    compound your savings and get on the road faster!
+                  </p>
+                </Modal.Body>
+              </Modal>
+            </div>
+            {/* 3//////// */}
+            <div>
+              <button onClick={handleShow} className="tipsButton">
+                <span>HOW TO PLAN YOUR TRIP</span>
+              </button>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Body>
+                  <div className="d-flex justify-content-center">
+                    <i
+                      style={{ fontSize: "60px" }}
+                      className="my-3 far fa-lightbulb"
+                    ></i>
+                  </div>
+                  <p
+                    className="px-5 pb-4"
+                    style={{ fontSize: "20px", textAlign: "justify" }}
+                  >
+                    Planning your trip can be a lot of work! Where do you start?
+                    What’s step one? What’s step two? What’s step three? Fret
+                    not! This page will give you all the best resources I have
+                    for planning your trip from start to finish. These posts go
+                    into the nuts and bolts of planning. They will teach you
+                    what to do first, second, and third. They will teach you how
+                    to narrow down your travel choices, know where to get legit
+                    information on the web (besides here), and help you avoid
+                    the newbie mistakes I made in the beginning!
+                  </p>
+                </Modal.Body>
+              </Modal>
+            </div>
+            {/* 4//////// */}
+            <div>
+              <button onClick={handleShow} className="tipsButton">
+                <span>GET THE RIGHT GEAR</span>
+              </button>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Body>
+                  <div className="d-flex justify-content-center">
+                    <i
+                      style={{ fontSize: "60px" }}
+                      className="my-3 far fa-lightbulb"
+                    ></i>
+                  </div>
+                  <p
+                    className="px-5 pb-4"
+                    style={{ fontSize: "20px", textAlign: "justify" }}
+                  >
+                    Outside a few key things, I don’t think travelers don’t need
+                    a lot of gear when they travel (unless you’re hiking and
+                    camping). The more you carry, the more you’re weighed down.
+                    However, since gear is expensive, making a mistake or two
+                    can lead you to wasting money, buying the wrong gear, or
+                    traveling with too much stuff. This section will teach you
+                    how to buy the right backpack, what to pack, protect your
+                    data and teach, and give you some other advanced tips and
+                    tricks for the road!
+                  </p>
+                </Modal.Body>
+              </Modal>
+            </div>
           </aside>
         </Col>
       </Row>
