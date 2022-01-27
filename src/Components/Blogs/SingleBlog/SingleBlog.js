ba4ce8f3de0ea47f8fb82b73ff923169825e2306
avishekataur <avishekataur@gmail.com>
2022-01-27 19:20:36 +0600

    display_single_blog_done

--- src/Components/Blogs/SingleBlog/SingleBlog.js
+++ src/Components/Blogs/SingleBlog/SingleBlog.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Col,Container } from "react-bootstrap";
-import "../SingleBlog/SingleBlog.css";
+import { Col, Container } from "react-bootstrap";
+import useAuth from "../../../Hooks/useAuth";
+import { Link } from "react-router-dom";
+
 const SingleBlog = () => {
+  const { user } = useAuth();
   const { id } = useParams();
   const [singleBlog, setSingleBlog] = useState([]);
   useEffect(() => {
@@ -11,26 +14,43 @@ const SingleBlog = () => {
       .then((data) => setSingleBlog(data));
   }, [id]);
   return (
-    <div>
-      <h2 className="my-5 text-uppercase text-center px-3">
+    <div className="pb-5 mb-3">
+      <h2 className="my-5 text-uppercase text-center px-5">
         {singleBlog.blogTitle}
       </h2>
       <Col>
         <Container className="d-flex justify-content-center">
           <img
-          id="blogImg"
-            height="350"
-            width="700"
+            //   id="blogImg"
+            className="w-75"
             src={singleBlog.blogImg}
             alt=""
           />
         </Container>
-        <p
-          style={{ fontSize: "25px", color: "black" }}
-          className="w-75 mx-auto text-justify py-5"
-        >
-          {singleBlog.blogDescription}
-        </p>
+        <Container className="w-75">
+          <p
+            style={{ fontSize: "20px", color: "black" }}
+            className="mx-auto text-justify pt-5"
+          >
+            {singleBlog.blogDescription}
+          </p>
+          <div className="my-5">
+            <img
+              className="rounded-circle user-img me-2"
+              width={42}
+              height={42}
+              src={user.photoURL}
+              alt=""
+            />
+            <span className="text-uppercase">{user.displayName}</span>
+          </div>
+          <Link
+            className="pt-5 text-decoration-none text-danger"
+            to="/home"
+          >
+            Back To Home
+          </Link>
+        </Container>
       </Col>
     </div>
   );
